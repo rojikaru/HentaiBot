44dfa076d287b819ba2f8b1cf2f1f49faef98832
Alex Ivanov <oleksii120@gmail.com>
2024-01-24 11:55:01 +0200

    Added hentai bot class Co-authored-by: AndrewVynnytskyi <nikoljavin@gmail.com>

---
+++ src/main/java/org/hentaibot/HentaiBot.java
@@ -0,0 +1,9 @@
+package org.hentaibot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class HentaiBot /*extends TelegramLongPollingBot*/ {
+
+
+}
